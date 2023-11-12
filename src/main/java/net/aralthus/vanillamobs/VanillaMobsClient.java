614cda80fb73a56569a5bc02ff5a4be8b9428497
Aralthus <51178809+Sirbucket@users.noreply.github.com>
2023-11-12 16:05:46 -0500

    redo

---
+++ src/main/java/net/aralthus/vanillamobs/VanillaMobsClient.java
@@ -0,0 +1,10 @@
+package net.aralthus.vanillamobs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaMobsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
