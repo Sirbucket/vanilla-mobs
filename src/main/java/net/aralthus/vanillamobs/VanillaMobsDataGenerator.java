be0da4f773acbcbc0cfcf06504d58f900b7dd9d8
Aralthus <51178809+Sirbucket@users.noreply.github.com>
2023-11-12 15:42:54 -0500

    fix

---
+++ src/main/java/net/aralthus/vanillamobs/VanillaMobsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aralthus.vanillamobs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaMobsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
